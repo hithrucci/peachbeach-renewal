3c7de52f1b2d0c8f9b489370e6aa56eaacefac68
BuildTools <unconfigured@null.spigotmc.org>
2026-09-21 00:01:34 +0900

    section1&2 added

--- script/custom.js
+++ script/custom.js
@@ -1,74 +1,186 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // -----------------------------------
+// visual scroll
+// -----------------------------------
+
+function initVisualScroll() {
+  const visualTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#visual",
+      start: "top top",
+      end: "+=1500",
+      scrub: 1,
+      //   markers: true,
+    },
+  });
+
+  visualTl
+    .to("#visual .video_wrap", {
+      width: "100%",
+      height: "calc(100vh - 80px)",
+      duration: 1,
+      ease: "none",
+    })
+    .to(
+      "#visual .visual_card li",
+      {
+        opacity: 0,
+        duration: 0.3,
+        ease: "none",
+      },
+      0.7,
+    )
+    .to(
+      {},
+      {
+        duration: 1.5,
+      },
+    );
+}
+
+// -----------------------------------
 // intro
 // -----------------------------------
 
-const wavePath = document.querySelector("#intro .intro_wave path");
-const waveLength = wavePath.getTotalLength();
+const introSkip = false;
 
-// wave 초기 상태
-gsap.set(wavePath, {
-  strokeDasharray: waveLength,
-  strokeDashoffset: waveLength,
-});
+if (introSkip) {
+  gsap.set("#intro", {
+    display: "none",
+  });
 
-// logo 초기 상태
-gsap.set("#intro .intro_logo", {
-  opacity: 0,
-});
+  document.body.style.overflow = "auto";
 
-// text1, text2 초기 상태
-gsap.set(["#intro .intro_logo_text1", "#intro .intro_logo_text2"], {
-  clipPath: "inset(0 100% 0 0)",
-});
+  gsap.set("#visual .visual_card li", {
+    opacity: 1,
+  });
 
-// intro timeline
-const introTl = gsap.timeline();
+  initVisualScroll();
+} else {
+  const wavePath = document.querySelector("#intro .intro_wave path");
+  const waveLength = wavePath.getTotalLength();
 
-introTl
-  // wave
-  .to(
-    wavePath,
-    {
-      strokeDashoffset: 0,
-      duration: 2.5,
-      ease: "power2.inOut",
-    },
-    0,
-  )
+  gsap.set(wavePath, {
+    strokeDasharray: waveLength,
+    strokeDashoffset: waveLength,
+  });
 
-  // logo
-  .to(
-    "#intro .intro_logo",
-    {
-      opacity: 1,
-      duration: 1,
-      ease: "power1.out",
-    },
-    0,
-  )
-
-  // text1, text2
-  .to(
-    ["#intro .intro_logo_text1", "#intro .intro_logo_text2"],
-    {
-      clipPath: "inset(0 0% 0 0)",
-      duration: 1.5,
-      ease: "power2.inOut",
-    },
-    0,
-  )
-
-  // intro 전체 fade out
-  .to("#intro", {
+  gsap.set("#intro .intro_logo", {
     opacity: 0,
-    duration: 1,
-    ease: "power2.inOut",
-    delay: 0.5,
+  });
+
+  gsap.set(["#intro .intro_logo_text1", "#intro .intro_logo_text2"], {
+    clipPath: "inset(0 100% 0 0)",
+  });
+
+  gsap.set("header", {
+    yPercent: -100,
+  });
+
+  gsap.set("#visual .visual_card li", {
+    opacity: 0,
+  });
+
+  const introTl = gsap.timeline({
     onComplete: () => {
       gsap.set("#intro", {
         display: "none",
       });
+
+      document.body.style.overflow = "auto";
+
+      initVisualScroll();
     },
   });
+
+  introTl
+
+    // wave
+    .to(
+      wavePath,
+      {
+        strokeDashoffset: 0,
+        duration: 2.5,
+        ease: "power2.inOut",
+      },
+      0,
+    )
+
+    // logo
+    .to(
+      "#intro .intro_logo",
+      {
+        opacity: 1,
+        duration: 1,
+        ease: "power1.out",
+      },
+      0,
+    )
+
+    // logo text
+    .to(
+      ["#intro .intro_logo_text1", "#intro .intro_logo_text2"],
+      {
+        clipPath: "inset(0 0% 0 0)",
+        duration: 1.5,
+        ease: "power2.inOut",
+      },
+      0,
+    )
+
+    // intro 사라짐
+    .to("#intro", {
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.inOut",
+      delay: 0.2,
+    })
+
+    // header 등장
+    .to(
+      "header",
+      {
+        yPercent: 0,
+        duration: 1,
+        ease: "power2.out",
+      },
+      "<0.3",
+    )
+
+    .to(
+      "#visual .visual_card li",
+      {
+        opacity: 1,
+        duration: 1,
+        stagger: {
+          each: 0.3,
+          from: "random",
+        },
+        ease: "power2.out",
+      },
+      "<",
+    );
+}
+// -----------------------------------
+// section1
+// -----------------------------------
+
+gsap.fromTo(
+  "#section1 .mainText h2 span",
+  {
+    clipPath: "inset(0 100% 0 0)",
+  },
+  {
+    clipPath: "inset(0 0% 0 0)",
+    duration: 3,
+    ease: "power3.inOut",
+
+    scrollTrigger: {
+      trigger: "#section1 .mainText h2",
+      start: "top 70%",
+      toggleActions: "play none none reverse",
+      markers: true,
+    },
+  },
+);
